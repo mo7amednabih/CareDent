3f28b15e32cf05a418ebefb64a7a725c4fc8a9e2
mo7amednabih <eid46060@gmail.com>
2025-03-01 15:19:54 +0200

    update

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -15,7 +15,7 @@ const sendEmail = async (options) => {
 
   // 2) Define email options (like from, to, subject, email content)
   const mailOpts = {
-    from: "Heml <nabihmohaned@gmail.com>",
+    from: "CareDent <nabihmohaned@gmail.com>",
     to: options.Email,
     subject: options.subject,
     text: options.message,
