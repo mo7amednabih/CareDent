30f583ed134fcf309294a73f0bd6a06016091c91
mo7amednabih <eid46060@gmail.com>
2025-03-30 13:55:42 +0200

    update

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const { createOrder, getMyOrders } = require("../services/orderService");
 
+const { createOrderValidator } = require("../utils/validators/orderValidator");
+
 const { protect, allowedTo } = require("../services/authService");
 const router = express.Router();
 
 router.use(protect, allowedTo("user"));
 
 router.get("/", getMyOrders);
-router.post("/", createOrder);
+router.post("/", createOrderValidator, createOrder);
 
 module.exports = router;
---
+++ utils/validators/orderValidator.js
@@ -0,0 +1,32 @@
+const { check } = require("express-validator");
+const validatorMiddleware = require("../../middleware/validatorMiddleware");
+const Order = require("../../models/orderDoctorModel");
+
+exports.createOrderValidator = [
+  check("type")
+    .notEmpty()
+    .withMessage("Type is required")
+    .isIn([
+      "Tooth Extraction",
+      "Veneers",
+      "Root Canal Treatment",
+      "Dental Filling",
+      "Scaling & Polishing",
+      "Orthodontics (Braces)",
+    ])
+    .withMessage("Invalid type value"),
+
+  check("type").custom(async (value, { req }) => {
+    const existingOrder = await Order.findOne({
+      user: req.user._id,
+      type: value,
+      status: "upComing",
+    });
+
+    if (existingOrder) {
+      throw new Error(`You already have an upcoming appointment for ${value}`);
+    }
+  }),
+
+  validatorMiddleware,
+];
